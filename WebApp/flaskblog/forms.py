225059588ff258cd52eba559f3e6c5dbd7c88579
rihib <76939037+rihito-bannai@users.noreply.github.com>
2021-07-04 16:09:55 +0900

    Update forms.py

--- WebApp/flaskblog/forms.py
+++ WebApp/flaskblog/forms.py
@@ -7,19 +7,19 @@ from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
+    username = StringField('ユーザーネーム',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('パスワード', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    submit = SubmitField('新規登録')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+            raise ValidationError('このユーザーネームは既に使われています。')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
@@ -28,32 +28,32 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',
+    email = StringField('メールアドレス',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    password = PasswordField('パスワード', validators=[DataRequired()])
+    remember = BooleanField('パスワードを保存')
+    submit = SubmitField('ログイン')
 
 
 class UpdateAccountForm(FlaskForm):
-    username = StringField('Username',
+    username = StringField('ユーザーネーム',
                            validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
+    email = StringField('メールアドレス',
                         validators=[DataRequired(), Email()])
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
-    submit = SubmitField('Update')
+    picture = FileField('プロフィール画像をアップデート', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('アップデート')
 
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError('That username is taken. Please choose a different one.')
+                raise ValidationError('このユーザーネームは既に使われています。')
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError('That email is taken. Please choose a different one.')
+                raise ValidationError('このメールアドレスは既に使われています。')
 
 
 class PostForm(FlaskForm):
